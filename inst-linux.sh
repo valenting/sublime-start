634aeccc2e6f6e19b411ccec37d46d2e08a6c70d
Valentin Gosu <valentin.gosu@gmail.com>
2013-10-01 15:13:12 +0300

    Modified inst-linux.sh

---
+++ inst-linux.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+MACHINE_TYPE=`uname -m`
+if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+  tar jxf bin/linux/Sublime\ Text\ 2.0.2\ x64.tar.bz2
+else
+  tar jxf bin/linux/Sublime\ Text\ 2.0.2.tar.bz2
+fi
+
+sudo rm -rf /opt/sublime2
+sudo mv Sublime\ Text\ 2 /opt/sublime2
+sudo chmod -R 777 /opt/sublime2
+
+rm -f /usr/local/bin/subl
+ln -s /opt/sublime2/sublime_text /usr/local/bin/subl
+
+mkdir -p ~/.config/sublime-text-2/Packages/User/
+cp config/Preferences.sublime-settings ~/.config/sublime-text-2/Packages/User/
+cp config/user.sublime-keymap ~/.config/sublime-text-2/Packages/User/Default\ \(Linux\).sublime-keymap
+
